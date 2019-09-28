4c72216eaf3f1eb8522e649db49dddf792d4997d
jsun6065 <jsun6065@gmail.com>
2019-09-28 00:40:09 -0400

    init

---
+++ android/app/src/main/kotlin/jsun6065/github/io/medhack_2019_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package jsun6065.github.io.medhack_2019_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
